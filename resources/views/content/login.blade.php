4c637da645a71934a077adc36ad7e20ea62681ce
SKsergio <sergiofloress911@gmail.com>
2024-07-11 15:33:38 -0600

    ultimo commit del dia

---
+++ resources/views/content/login.blade.php
@@ -0,0 +1,14 @@
+@extends('templates.App') {{--extendiendo la plantilla o layaout--}}
+
+{{-- title --}}
+@section('title', 'login')
+
+{{-- links de css --}}
+@push('css')
+    <h2>Holaaaaaaa</h2>
+@endpush
+
+
+@section('content')
+    
+@endsection
